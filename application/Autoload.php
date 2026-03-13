58da4b7629604d7d1706eb11cb0349f11c2f1c9d
MayVan532 <mayravanessamorales1@gmail.com>
2026-03-13 16:14:29 -0600

    autoload

--- application/Autoload.php
+++ application/Autoload.php
@@ -7,8 +7,23 @@ function autoloadCore($class){
 }
 //cargar las librerias de forma automatica
 function autoloadLibs($class){ 
-    if(file_exists(ROOT . 'libs' . DS . 'class.' . strtolower($class) . '.php')){
-        include_once ROOT . 'libs' . DS . 'class.' . strtolower($class) . '.php';
+    // Ruta en minúsculas (comportamiento original)
+    $lower = ROOT . 'libs' . DS . 'class.' . strtolower($class) . '.php';
+    if (file_exists($lower)) {
+        include_once $lower;
+        return;
+    }
+    // Ruta respetando el nombre de la clase (por si el archivo está con mayúsculas)
+    $asIs = ROOT . 'libs' . DS . 'class.' . $class . '.php';
+    if (file_exists($asIs)) {
+        include_once $asIs;
+        return;
+    }
+    // Ruta con primera letra mayúscula por compatibilidad
+    $ucfirst = ROOT . 'libs' . DS . 'class.' . ucfirst(strtolower($class)) . '.php';
+    if (file_exists($ucfirst)) {
+        include_once $ucfirst;
+        return;
     }
 }
 
